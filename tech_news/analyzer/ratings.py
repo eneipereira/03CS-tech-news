fe469d7b9aa81a663a2c5b4ee40804347d654bf9
Enei <eneipereira@outlook.com.br>
2022-11-09 02:48:31 -0300

    feat: Implementa a função "top_5_categories", que lista as cinco categorias mais populares, baseada na quantidade de ocorrências desta.

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+from collections import Counter
 
 
 # Requisito 10
@@ -19,3 +20,14 @@ def top_5_news():
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+    all_news = find_news()
+
+    categories = [news["category"] for news in all_news]
+
+    sorted_categories = sorted(categories, key=lambda x: x)
+
+    top_5_categories = [
+        category[0] for category in Counter(sorted_categories).most_common(5)
+        ]
+
+    return top_5_categories
