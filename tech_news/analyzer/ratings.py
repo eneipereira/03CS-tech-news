91750dbb4ce84e3b93aa2bfee5ae677e51035564
Enei <eneipereira@outlook.com.br>
2022-11-09 01:26:14 -0300

    feat: Implementa a função "top_5_news", que lista as cinco notícias mais populares, baseada na quantidade de comentários.

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,19 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    all_news = find_news()
+
+    sorted_news = sorted(
+        all_news,
+        key=lambda x: (-x["comments_count"], x["title"])
+        )
+
+    top_5_news = [(news["title"], news["url"]) for news in sorted_news][:5]
+
+    return top_5_news
 
 
 # Requisito 11
