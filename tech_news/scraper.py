015788ba80c8417bad57572319341f7344bbdd57
Enei <eneipereira@outlook.com.br>
2022-11-08 22:48:50 -0300

    feat: Implementa a função "get_tech_news", que busca a quantidade de notícias recebidas como argumento, insere-as no banco e retorna as mesmas.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 from requests import get, ReadTimeout, HTTPError
 from ratelimiter import RateLimiter
 from parsel import Selector
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -64,3 +65,23 @@ def scrape_noticia(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+    url_to_scrape = "https://blog.betrybe.com"
+
+    all_news = []
+
+    while len(all_news) <= amount:
+        html_content = fetch(url_to_scrape)
+        page_urls = scrape_novidades(html_content)
+
+        for url in page_urls:
+            curr_html_content = fetch(url)
+            news = scrape_noticia(curr_html_content)
+            all_news.append(news)
+
+        url_to_scrape = scrape_next_page_link(html_content)
+
+    all_news = all_news[:amount]
+
+    create_news(all_news)
+
+    return all_news
